06fe116367cdc2a7e562d1e928fd16f445084f7c
Senthil Basuva Raj <senthil.basuvaraj@corefactors.in>
2013-04-26 10:05:08 +0530

    Sample Application Flow
    Sample Application Flow

---
+++ fbpm_cs/fbpm_cs/Controllers/PaymentScheduleController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fbpm_cs.Models;
+
+namespace fbpm_cs.Controllers
+{
+    public class PaymentScheduleController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var entities = new fbpmEntities();
+            Console.WriteLine("Model Count:" + entities.PaymentSchedules.Count());
+            return View(entities.PaymentSchedules);
+        }
+
+        //
+        // GET: /PaymentSchedule/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /PaymentSchedule/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /PaymentSchedule/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /PaymentSchedule/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /PaymentSchedule/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /PaymentSchedule/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /PaymentSchedule/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ fbpm_cs/fbpm_cs/Controllers/SampleController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fbpm_cs.Models;
+
+namespace fbpm_cs.Controllers
+{ 
+    public class SampleController : Controller
+    {
+        private fbpmSample db = new fbpmSample();
+
+        //
+        // GET: /Sample/
+
+        public ViewResult Index()
+        {
+            return View(db.Samples.ToList());
+        }
+
+        //
+        // GET: /Sample/Details/5
+
+        public ViewResult Details(Guid id)
+        {
+            Sample sample = db.Samples.Single(s => s.SampleID == id);
+            return View(sample);
+        }
+
+        //
+        // GET: /Sample/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Sample/Create
+
+        [HttpPost]
+        public ActionResult Create(Sample sample)
+        {
+            if (ModelState.IsValid)
+            {
+                sample.SampleID = Guid.NewGuid();
+                db.Samples.AddObject(sample);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(sample);
+        }
+        
+        //
+        // GET: /Sample/Edit/5
+ 
+        public ActionResult Edit(Guid id)
+        {
+            Sample sample = db.Samples.Single(s => s.SampleID == id);
+            return View(sample);
+        }
+
+        //
+        // POST: /Sample/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Sample sample)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Samples.Attach(sample);
+                db.ObjectStateManager.ChangeObjectState(sample, EntityState.Modified);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sample);
+        }
+
+        //
+        // GET: /Sample/Delete/5
+ 
+        public ActionResult Delete(Guid id)
+        {
+            Sample sample = db.Samples.Single(s => s.SampleID == id);
+            return View(sample);
+        }
+
+        //
+        // POST: /Sample/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {            
+            Sample sample = db.Samples.Single(s => s.SampleID == id);
+            db.Samples.DeleteObject(sample);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
